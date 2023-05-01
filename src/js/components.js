a9e34f6e95b94a644f5f5aeb29961a250d044d51
Ran Kirlian <rankirlian@gmail.com>
2023-05-01 21:27:35 +0200

    Add components.css and import them in components.js

--- src/js/components.js
+++ src/js/components.js
@@ -1,8 +1,10 @@
-export const displayMessage = (name) => { 
+import '../css/components.css';
+
+export const displayMessage = (name) => {
   console.log('Entering the function');
 
-  const h2 = document.createElement('h2')
-  h2.innerText= `My name is ${name}`;
+  const h2 = document.createElement('h2');
+  h2.innerText = `My name is ${name}`;
 
   document.body.append(h2);
- }
+};
