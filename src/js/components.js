bd55296b9f3c2463042a3bec3703adf56abe80c1
Ran Kirlian <rankirlian@gmail.com>
2023-05-01 23:34:31 +0200

    Add components.css and js to import the img and style it

--- src/js/components.js
+++ src/js/components.js
@@ -1,10 +1,14 @@
 import '../css/components.css';
+import catImg from '../assets/img/cat.jpg';
 
 export const displayMessage = (name) => {
   console.log('Entering the function');
 
   const h2 = document.createElement('h2');
   h2.innerText = `My name is ${name}`;
-
   document.body.append(h2);
+
+  const img = document.createElement('img');
+  img.src = catImg;
+  document.body.append(img);
 };
