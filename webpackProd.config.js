28219ac9ec8c3ec99471b51ff4ca9cc2038f2453
Ran Kirlian <rankirlian@gmail.com>
2023-05-06 20:37:53 +0200

    Update webpack prod config to add optimizators config

--- webpackProd.config.js
+++ webpackProd.config.js
@@ -1,25 +1,63 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const CssMinimizer = require('css-minimizer-webpack-plugin');
+const Terser = require('terser-webpack-plugin');
+
 module.exports = {
   mode: 'production',
   output: {
     clean: true, // delete all dist files on build creation
+    filename: 'main.[contenthash].js',
   },
   module: {
     rules: [
       {
-        // Handle all html files to run them with html-loader
+        // To handle all html files to run them with html-loader
         test: /\.html$/,
         loader: 'html-loader',
         options: {
           sources: false,
         },
       },
+      {
+        // To handle local css styles first
+        test: /\.css$/,
+        exclude: /styles.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        // To handle global css styles
+        test: /styles.css$/,
+        use: [MiniCssExtract.loader, 'css-loader'],
+      },
+      {
+        // To handle asset type files (jpg, png & gif by pattern)
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'file-loader',
+      },
     ],
   },
-  optimization: {},
+  optimization: {
+    // Prod optimizations
+    minimize: true,
+    minimizer: [new CssMinimizer(), new Terser()],
+  },
   plugins: [
     new HtmlWebPackPlugin({
+      // HTNL handling
       title: 'Basic webpack setup',
       filename: 'index.html',
+    }),
+    new MiniCssExtract({
+      // Global css styles
+      filename: 'style.[fullhash].css',
+      ignoreOrder: false,
+    }),
+    new CopyPlugin({
+      // File handling
+      patterns: [{ from: 'src/assets/', to: 'assets/' }],
     }),
   ],
 };
